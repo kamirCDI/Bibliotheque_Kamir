c33755ab11dbe3ffe23ce01a9c97cf59dbf1a665
Sirendjivi BARRES <sirendjivibarres@hotmail.com>
2015-04-08 16:25:09 +0200

    Mise à jour des classes Exemplaire et EmpruntArchive

--- src/biblio/metier/Exemplaire.java
+++ src/biblio/metier/Exemplaire.java
@@ -1,5 +1,6 @@
 package biblio.metier;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Exemplaire {
@@ -12,6 +13,26 @@ public class Exemplaire {
 	private static class EmpruntEnCours{
 		private Date dateEmprunt;
 		private Utilisateur emprunteur;
+		private Exemplaire exemplaire;
+		
+		public Date getDateEmprunt() {
+			return dateEmprunt;
+		}
+		public void setDateEmprunt(Date dateEmprunt) {
+			this.dateEmprunt = dateEmprunt;
+		}
+		public Utilisateur getEmprunteur() {
+			return emprunteur;
+		}
+		public void setEmprunteur(Utilisateur emprunteur) {
+			this.emprunteur = emprunteur;
+		}
+		public Exemplaire getExemplaire() {
+			return exemplaire;
+		}
+		public void setExemplaire(Exemplaire exemplaire) {
+			this.exemplaire = exemplaire;
+		}
 	}
 
 	public int getIdExemplaire() {
