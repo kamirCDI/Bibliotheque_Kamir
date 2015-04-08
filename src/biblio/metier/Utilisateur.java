4f35e487296443dfdc4c0893f303d0f8f4f6146b
Kamir <afpa@192.168.101.175>
2015-04-08 13:59:42 +0200

    classe Utilisateur en cours de création

--- src/biblio/metier/Utilisateur.java
+++ src/biblio/metier/Utilisateur.java
@@ -1,12 +1,23 @@
-/**
- * 
- */
+
 package biblio.metier;
+
+import java.text.ParseException;
 
 /**
  * @author afpa
  *
  */
-public class Utilisateur {
+public class Utilisateur extends Personne {
+	
+	private int idUtilisateur;
+	private String pwd;
+	private String pseudonyme;
+
+
+	public Utilisateur() throws ParseException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
 
 }
