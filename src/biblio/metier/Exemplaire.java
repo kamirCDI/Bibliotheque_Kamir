33db034b245c485f7dc22f9c4fd980516c1f7d64
Sirendjivi BARRES <sirendjivibarres@hotmail.com>
2015-04-08 15:40:08 +0200

    toString() sur exemplaire

--- src/biblio/metier/Exemplaire.java
+++ src/biblio/metier/Exemplaire.java
@@ -3,6 +3,7 @@ package biblio.metier;
 import java.util.Date;
 
 public class Exemplaire {
+	
 	private int idExemplaire;
 	private Date dateAchat;
 	private EnumStatusExemplaire status;
@@ -11,6 +12,44 @@ public class Exemplaire {
 	private static class EmpruntEnCours{
 		private Date dateEmprunt;
 		private Utilisateur emprunteur;
+	}
+
+	public int getIdExemplaire() {
+		return idExemplaire;
+	}
+
+	public void setIdExemplaire(int idExemplaire) {
+		this.idExemplaire = idExemplaire;
+	}
+
+	public Date getDateAchat() {
+		return dateAchat;
+	}
+
+	public void setDateAchat(Date dateAchat) {
+		this.dateAchat = dateAchat;
+	}
+
+	public EnumStatusExemplaire getStatus() {
+		return status;
+	}
+
+	public void setStatus(EnumStatusExemplaire status) {
+		this.status = status;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	@Override
+	public String toString() {
+		return "Exemplaire [idExemplaire=" + idExemplaire + ", dateAchat="
+				+ dateAchat + ", status=" + status + ", isbn=" + isbn + "]";
 	}
 	
 	
